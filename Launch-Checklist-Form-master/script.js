1306bc45ca9f1dc32a906523ad41a8178af24d9f
Sabrina Koumoin <sak96528@marymount.edu>
2021-01-10 22:49:01 +0000

    done

--- Launch-Checklist-Form-master/script.js
+++ Launch-Checklist-Form-master/script.js
@@ -14,12 +14,14 @@
 
 window.addEventListener("load", function(){
    let form = document.querySelector("form");
+
    form.addEventListener("submit", function(event){
+       event.preventDefault();
        let pilotName = document.querySelector("input[name=pilotName]");
        let copilotName = document.querySelector("input[name=copilotName]");
        let fuelLevel = document.querySelector("input[name=fuelLevel]");
        let cargoMass = document.querySelector("input[name=cargoMass]");
-       
+       let missionTarget =  document.getElementById("missionTarget")
        let launchStatusCheck= document.getElementById("launchStatusCheck")
        let lauchStatus = document.getElementById("launchStatus");
        let faultyItems = document.getElementById("faultyItems")
@@ -27,43 +29,62 @@ window.addEventListener("load", function(){
        let copilotStatus = document.getElementById("copilotStatus");
        let fuelStatus = document.getElementById("fuelStatus");
        let cargoStatus = document.getElementById("cargoStatus");
+       let ready = true;
        if (pilotName.value===""|| copilotName.value===""||fuelLevel.value===""||cargoMass.value===""){
            alert("All values must be entered");
-         event.preventDefault();
        }
-       else if (isNaN(Number(fuelLevel.value), Number(cargoMass.value))){
+       if (isNaN(Number(fuelLevel.value), Number(cargoMass.value))){
          alert("Fuel Level and Cargo Mass entries must be a number")
-       event.preventDefault(); 
        }
-       else {
+        if (fuelLevel.value <= 10000 ){
+            
+           faultyItems.style.visibility= "visible" 
+            fuelStatus.innerHTML = "Fuel Level too low for launch"
+            launchStatus.innerHTML="Shuttle not ready for launch"
+           launchStatus.style.color = "red"
+           ready = false;
+      }
+
+        if (cargoMass.value >= 10000){
+          
+            faultyItems.style.visibility= "visible"
+           
+            cargoStatus.innerHTML = "Cargo Mass too high for launch"
+            launchStatus.innerHTML= "Shuttle not ready for launch"
+          launchStatus.style.color = "red"
+          ready = false;
+        }
+
+        if(ready){
           faultyItems.style.visibility= "visible"
-          pilotStatus.innerHTML(`Pilot, ${pilotName.value}, is not ready`);
-          copilotStatus.innerHTML(`Co-Pilot, ${copilotName.value}, is not ready`);
-          if(fuelStatus < 10,000){
-             return ("Fuel level too low for launch")
-          }
-          if (cargoMass > 10,000){
-             return ("Cargo mass too high for launch")
-          }
-          launchStatus.innerHTML("Shuttle not ready for launch")
-          launchStatus.style.font = "red"
+          launchStatus.innerHTML="Shuttle is ready for launch"
+          launchStatus.style.color = "green"
        }
-      //faultyItems.style.visibility= "visible"
-      // pilotStatus.innerHTML(`Pilot, ${pilotName.value}, is ready`)
-      // copilotName.innerHTML(`Co-Pilot, ${copilotName.value}, is ready`)
-      // if(fuelStatus > 10,000){
-      //    return ("Fuel level high enough for launch")
-      // }
-      // if (cargoMass < 10,000){
-      //    return ("Cargo mass low enough for launch")
-      // }
-      // launchStatus.innerHTML("Shuttle is ready for launch")
-      // launchStatus.style.font = "green"
+  
 
    })
+   
    fetch ('https://handlers.education.launchcode.org/static/planets.json')
       .then (function(response){
-         return response.json()
+        response.json().then(function(json) {
+                let planets = json
+        let index = Math.floor(Math.random()*planets.length);
+        let planet = planets[index]
+
+        let template = `
+        <h2>Mission Destination</h2>
+            <ol>
+            <li>Name: ${planet.name} </li>
+            <li>Diameter: ${planet.diameter}</li>
+            <li>Star: ${planet.star}</li>
+            <li>Distance from Earth: ${planet.distanceFromEarth}</li>
+            <li>Number of Moons:${planet.numberOfMoons}</li>
+            </ol>
+            <img src="${planet.image}">`
+        missionTarget.innerHTML = template
+        })
+        
+         
       })
       .then (function(data){
          console.log(data)
@@ -73,4 +94,3 @@ window.addEventListener("load", function(){
       })      
 })
 
-window.onload = init;
